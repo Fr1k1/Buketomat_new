08227345984c27cf26091bf909a8632fc3219042
fmilohano20 <fmilohano20@foi.hr>
2022-11-14 14:46:47 +0100

    Initial implementation of Flower.kt, Order.kt , Bouquet.kt This implementation will be upgraded down the line Also user will need to be added in Order.kt

---
+++ Software/app/src/main/java/com/example/buketomat/models/Order.kt
@@ -0,0 +1,14 @@
+package com.example.buketomat.models
+
+import java.util.Date
+
+class Order (
+
+    val Id : Int,
+    val OrderTime : Date,
+    val FinalPrice : Double,
+    val DeliveryTime : Date?,
+    val Bouquets : List<Bouquet>
+    //val User : User               --> add later when merged into main branch
+
+)

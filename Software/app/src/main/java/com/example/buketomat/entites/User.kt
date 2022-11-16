b12d5718e5328bc43c3cf2b303c0d1f13fe782f9
mfriscic20 <mfriscic20@student.foi.hr>
2022-11-16 21:44:41 +0100

    Implemented registration logic with mock data

--- Software/app/src/main/java/com/example/buketomat/entites/User.kt
+++ Software/app/src/main/java/com/example/buketomat/entites/User.kt
@@ -1,5 +1,9 @@
 package com.example.buketomat.entites
 
- class User(val email:String, val username:String, val password:String)
+class User(val email: String, val username: String, val password: String) {
+    override fun toString(): String {
+        return username
+    }
+}
 // ToDo ako je preporuka da klasa sadrzi podatke onda je data class...nakon prve faze dodaj sve podatke o korisniku
 

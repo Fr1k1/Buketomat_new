4ce5ca8f3db1a368de19e47ec6be1302f0a2ab45
fmilohano20 <fmilohano20@foi.hr>
2023-01-05 01:05:05 +0100

    Implemented final price calculation  and individual price calculation for shopping item - F#Z08

--- Software/app/src/main/java/com/example/buketomat/adapters/ShoppingCartAdapter.kt
+++ Software/app/src/main/java/com/example/buketomat/adapters/ShoppingCartAdapter.kt
@@ -7,14 +7,16 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.RecyclerView
 import com.example.buketomat.R
 import com.example.buketomat.backgroundworkers.BouquetClickListener
+import com.example.buketomat.fragments.ShopingCardFragment
 
 import com.example.buketomat.models.OrderBouquet
 import com.squareup.picasso.Picasso
 
-class ShoppingCartAdapter(private val shoppingItems: MutableList<OrderBouquet>,private val callback: BouquetClickListener) :
+class ShoppingCartAdapter(private val shoppingItems: MutableList<OrderBouquet>,private val callbackMain: BouquetClickListener, private val callbackShop : ShopingCardFragment) :
     RecyclerView.Adapter<ShoppingCartAdapter.ShoppingCartItemViewHolder>() {
 
 
@@ -36,11 +38,28 @@ class ShoppingCartAdapter(private val shoppingItems: MutableList<OrderBouquet>,p
                 etItemAmount = view.findViewById(R.id.etItemAmount)
                // kol = view.findViewById(R.id.kolicina)
 
+                etItemAmount.addTextChangedListener {
+                    if(etItemAmount.text.toString() != "")
+                    {
+                        try {
+                            val kolicina = etItemAmount.text.toString().toInt()
+                            shoppingItems[adapterPosition].kolicina = kolicina
+                            bouquetPrice.text =  (kolicina * shoppingItems[adapterPosition].Price).toString() + " EUR"
+                            callbackShop.calculateTotal();
+                        }
+                        catch(ex : NumberFormatException){
+                        }
+
+                    }
+
+                }
+
+
                 view.setOnLongClickListener{
                     AlertDialog.Builder(view.context)
                         .setTitle("Izbriši stavku")
                         .setPositiveButton(view.resources.getString(R.string.Delete)){ _, _ ->
-                            callback.removeBouquetFromOrder(shoppingItems[adapterPosition])
+                            callbackMain.removeBouquetFromOrder(shoppingItems[adapterPosition])
                             notifyDataSetChanged();
                         }
                         .setNegativeButton(view.resources.getString(R.string.Cancel)){ _, _ ->
--- Software/app/src/main/java/com/example/buketomat/fragments/ShopingCardFragment.kt
+++ Software/app/src/main/java/com/example/buketomat/fragments/ShopingCardFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.buketomat.MainActivity
@@ -16,8 +17,10 @@ import com.example.buketomat.models.OrderBouquet
 class ShopingCardFragment : Fragment() {
 
     private lateinit var rvShoppingItems : RecyclerView
+    private lateinit var tvFinalPrice : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -27,6 +30,18 @@ class ShopingCardFragment : Fragment() {
         activity.shoppingItems.forEach {
             Log.i("OrderItems",it.Name + " " + it.kolicina);
         }
+        createList();
+        calculateTotal();
+    }
+
+    public fun calculateTotal() {
+        tvFinalPrice = requireView().findViewById(R.id.tv_new_order_finalPrice)
+        var activity = activity as MainActivity
+        var total = 0.0;
+        activity.shoppingItems.forEach {
+            total+=it.kolicina * it.Price
+        }
+        tvFinalPrice.text = "UKUPNO: " + total + " EUR"
     }
 
     override fun onCreateView(
@@ -37,16 +52,12 @@ class ShopingCardFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_shoping_card, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        createList();
-    }
-
     private fun createList()
     {
         var activity = activity as MainActivity;
         rvShoppingItems = requireView().findViewById(R.id.rvShoppingItems)
         //val orderAdapter = OrdersAdapter(MockDataLoader.getDemoDataOrders())
-        val shoppingCartAdapter = ShoppingCartAdapter(activity.shoppingItems,activity)
+        val shoppingCartAdapter = ShoppingCartAdapter(activity.shoppingItems,activity,this)
         rvShoppingItems.layoutManager = LinearLayoutManager(requireView().context)
         rvShoppingItems.adapter = shoppingCartAdapter
     }
